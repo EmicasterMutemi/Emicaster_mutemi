b5adf5eaadf33b789fc64991b3a84975459c6ed6
EmicasterMutemi <emicastermutemi@gmail.com>
2025-03-05 00:50:22 +0300

    Add files via upload

---
+++ compound_interest.c
@@ -0,0 +1,23 @@
+//program to calculate compound interest 
+/*
+Author:Emicaster Mutemi 
+ADM:D33-2413-2023
+DATE:3/03/2025
+*/
+#include <stdio.h>
+#include<math.h>
+int main() 
+{
+    float principle, rate, time, CI;
+    //input 
+    
+    printf("enter principle:");
+    scanf("%f", &principle);
+    printf("enter time:");
+    scanf("%f", &time);
+    printf("enter rate:");
+    scanf("%f", &rate);
+    //calculate and display answer
+    
+    return 0;
+}    
---
+++ simple_interest.c
@@ -0,0 +1,24 @@
+//program to calculate simple interest 
+/*
+Author:Emicaster Mutemi 
+ADM:D33-2413-2023
+DATE:3/03/2025
+*/
+#include <stdio.h>
+
+int main() 
+{
+     int principle, interest_rate, time, simple_interest;
+     //Taking user input 
+     printf("enter the principle amount:");
+     scanf("%d", &principle);
+     printf("enter the rate of interest:");
+     scanf("%d", &interest_rate);
+     printf("enter the time:");
+     scanf("%d", &time);
+     //calculating simple interest 
+     //simple interest =(principle*rate*time)/100
+     //display the result 
+     
+     return 0;
+ }    
